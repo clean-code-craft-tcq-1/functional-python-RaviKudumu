6d57522534296e03ca0e1da3d82daa8ab7fde13b
Ravi Kudumu <ravikudumu@gmail.com>
2021-03-02 20:07:09 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,4 +1,3 @@
-Battery_range = [[0, 45], [20, 80], [-1, 0.8]]
 batter_parameter = {"temperature": [0,45,"Temperature"],
                     "soc": [20,80,"Soc"],
                     "charge_rate": [0, 0.8,"Charge Rate"]}
@@ -17,4 +16,5 @@ if __name__ == '__main__':
   assert (battery_status({"battery_parameter": "charge_rate", "value": 0.7}) is True)
   assert(battery_status({"battery_parameter": "temperature", "value": 50}) is False)
   assert (battery_status({"battery_parameter": "soc", "value": 85}) is False)
+  assert (battery_status({"battery_parameter": "soc", "value": 19}) is False)
   assert (battery_status({"battery_parameter": "charge_rate", "value": 0}) is False)
